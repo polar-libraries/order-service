fd43d20f628f511f59ee55cff3f70c4e84039573
erickknsilva <erick.nsilva100@gmail.com>
2025-04-03 00:49:09 -0300

    Implementação do flyway para gerenciamento e versionamento do banco de dados POSTGRES, criação da chamada REATIVA com WebClient para fazer uma ligação com o aplicativo CATALOG-SERVICE e ver a disponibilidades dos livros para realizar o pedido caso esteja disponivel, configurando variaveiis de propriedades para chamar armazenar a rota aplicativo de apoio. Entendendo a diferença entre chamadas Interativas e Reativas e utilizando o modelos de espelho de classe para obter o menor acoplamento de implantação possivel entre os dois aplicativos.

---
+++ src/main/java/com/polarbookshop/orderservice/domain/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.polarbookshop.orderservice.domain;
+
+public enum OrderStatus {
+    ACCEPTED, REJECTED, DISPATCHED
+}
---
+++ src/main/java/com/polarbookshop/orderservice/web/OrderController.java
@@ -0,0 +1,32 @@
+package com.polarbookshop.orderservice.web;
+
+import com.polarbookshop.orderservice.domain.Order;
+import com.polarbookshop.orderservice.domain.OrderService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RequestMapping("orders")
+@RestController
+public class OrderController {
+
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+
+    @GetMapping
+    public Flux<Order> getBookOrder() {
+        return orderService.getAllOrders();
+    }
+
+    @PostMapping
+    public Mono<Order> submitOrder(@Valid @RequestBody OrderRequest request) {
+        return orderService.submitOrder(request.isbn(), request.quantity());
+    }
+
+
+}
